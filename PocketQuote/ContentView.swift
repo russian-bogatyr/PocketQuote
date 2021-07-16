4622e73934029ddab165c7dc5f8283b918bc2093
Anton <kiselevtony@gmail.com>
2021-07-16 13:27:18 -0400

    Animation added plus splash screen

--- PocketQuote/ContentView.swift
+++ PocketQuote/ContentView.swift
@@ -11,27 +11,58 @@ struct ContentView: View {
     
     @ObservedObject var  networkManager = NetworkManager()
 
+    @State private var fadeInOut = false
     
     var body: some View {
         ZStack{
+            Color(red: 0, green: 0, blue: 0)
+                .ignoresSafeArea()
             VStack{
                 Spacer()
-                Text(networkManager.quoteText)
-                    .frame(width: UIScreen.main.bounds.size.width-50, height: UIScreen.main.bounds.size.height/2, alignment: .center)
-                    .padding(.all)
-                Text(networkManager.quoteAuthor)
-                    .padding()
-                
+                VStack{
+                    
+                    if fadeInOut{
+                        Text(networkManager.quoteText)
+                            .frame(width: UIScreen.main.bounds.size.width-50, height: UIScreen.main.bounds.size.height/2, alignment: .center)
+                            .padding(.all)
+                            .foregroundColor(.white)
+                            .font(.system(size: 20))
+                            .transition(AnyTransition.opacity.animation(.easeInOut(duration: 1.0)))
+                            
+                        Text(networkManager.quoteAuthor)
+                            .padding()
+                            .foregroundColor(.white)
+                            .transition(AnyTransition.opacity.animation(.easeInOut(duration: 1.5)))
+                    }
+                    
+                    if !fadeInOut{
+                        Text(networkManager.quoteText)
+                            .frame(width: UIScreen.main.bounds.size.width-50, height: UIScreen.main.bounds.size.height/2, alignment: .center)
+                            .padding(.all)
+                            .foregroundColor(.white)
+                            .font(.system(size: 20))
+                            .transition(AnyTransition.opacity.animation(.easeInOut(duration: 1.0)))
+                            
+                        Text(networkManager.quoteAuthor)
+                            .padding()
+                            .foregroundColor(.white)
+                            .transition(AnyTransition.opacity.animation(.easeInOut(duration: 1.5)))
+                    }
+                    
+                }
                 Spacer()
                 Button("Next Quote") {
+                    self.fadeInOut.toggle()
                     networkManager.fetchData()
                 }
+                .foregroundColor(.black)
                 .padding()
-                .background(Color(red: 0, green: 0, blue: 0.5))
+                .background(Color.yellow)
                 .clipShape(Capsule())
                 Spacer()
             }.onAppear{
                 self.networkManager.fetchData()
+                self.fadeInOut.toggle()
             }
         }
     }
